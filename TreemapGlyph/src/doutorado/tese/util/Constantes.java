f8b0d2a3058719cbe8e569d0e6a888eb9f644dbf
Anderson Soares <anderson.gmarques@gmail.com>
2018-02-24 21:07:01 -0300

    Alteraçõe nas texturas dos glyphs
    Inclui texturas novas, e alterei algumas que poderiam causar confusão nos usuários.

--- TreemapGlyph/src/doutorado/tese/util/Constantes.java
+++ TreemapGlyph/src/doutorado/tese/util/Constantes.java
@@ -20,44 +20,42 @@ public class Constantes {
     private static boolean showStarGlyph = false;
 //    private static Color[] cores = {Color.BLUE, Color.GREEN, Color.MAGENTA, Color.ORANGE, Color.PINK, Color.RED, Color.YELLOW};
     private static String[] cor = {
-        "#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", 
-        "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99", 
-        "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262", 
+        "#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6",
+        "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99",
+        "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262",
         "#5574a6", "#3b3eac", "#F0F8FF"};
 
     public final static String[] TIPO_TEXTURA = {
+        "PATTERN_ASTERISK",
         "PATTERN_DIAG1",
         "PATTERN_DIAG2",
+        "PATTERN_DIAG3",
+        "PATTERN_DIAG4",
+        "PATTERN_DIAG5",
         "PATTERN_DIAGDOTS",
+        "PATTERN_DIAGDOTS2",
         "PATTERN_DIAGS",
-        "PATTERN_DOTS",
-        "PATTERN_PLUS",
+        "PATTERN_DOTS ",
+        "PATTERN_PLUS ",
         "PATTERN_TUILES",
         "PATTERN_SQUARES",
         "PATTERN_LIGHT_GRAY",
-        "PATTERN_TIO",
-        "PATTERN_SS"
+        "PATTERN_TIO"
     };
     /**
-     * 0 - "OCTOGONO",
-     * 1 - "ELLIPSE", 
-     * 2 - "CRUZ", 
-     * 3 - "LOSANGO", 
-     * 4 - "PENTAGONO", 
-     * 5 - "TRAPEZIO", 
-     * 6 - "HEXAGONO",
-     * 7 - "RETANGULO", 
-     * 8 - "CIRCULO"
+     * 0 - "OCTOGONO", 1 - "ELLIPSE", 2 - "CRUZ", 3 - "LOSANGO", 4 -
+     * "PENTAGONO", 5 - "TRAPEZIO", 6 - "HEXAGONO", 7 - "RETANGULO", 8 -
+     * "CIRCULO"
      */
     public final static String[] TIPOS_FORMAS_GEOMETRICAS = {
-        "OCTOGONO", "ELLIPSE", "CRUZ", "LOSANGO", "PENTAGONO", "TRAPEZIO", "HEXAGONO", "RETANGULO", "CIRCULO" 
+        "OCTOGONO", "ELLIPSE", "CRUZ", "LOSANGO", "PENTAGONO", "TRAPEZIO", "HEXAGONO", "RETANGULO", "CIRCULO"
     };
-    
+
     public final static String[] LETRAS_ALFABETO = {
-        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
+        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
         "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
     };
-    
+
     public final static String[] NUMEROS = {
         "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"
     };
@@ -87,8 +85,10 @@ public class Constantes {
     }
 
     /**
-     * Vetor de cores usado no segundo nivel de glyphs e na dimensao cores do treemap
-     * @return 
+     * Vetor de cores usado no segundo nivel de glyphs e na dimensao cores do
+     * treemap
+     *
+     * @return
      */
     public static String[] getCor() {
         return cor;
